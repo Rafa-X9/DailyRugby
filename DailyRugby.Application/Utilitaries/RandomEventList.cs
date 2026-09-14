f673d28e5f73605e23b0ccea11d14a686ec532bc
RafaX9 <rafaeldealencarfloriano@gmail.com>
2026-09-14 18:49:32 -0400

    Added the RandomEventList data structure and its unit test

---
+++ DailyRugby.Application/Utilitaries/RandomEventList.cs
@@ -0,0 +1,57 @@
+﻿namespace DailyRugby.Application.Utilitaries;
+
+public class RandomEventList<T>(Random random)
+{
+    private readonly List<Event> _events = [];
+    private Func<T>? _fallback;
+    private readonly Random _random = random;
+
+    public RandomEventList<T> Add(double chance, Func<T> eventAction)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(chance);
+        _events.Add(new Event(chance, eventAction));
+        return this;
+    }
+
+    public RandomEventList<T> AddFallback(Func<T> fallback)
+    {
+        _fallback = fallback;
+        return this;
+    }
+
+    public T Draw()
+    {
+        if (_events.Count == 0)
+        {
+            if (_fallback is not null)
+                return _fallback();
+
+            throw new InvalidOperationException("The random event list contains no events or fallback.");
+        }
+
+        double totalChance = _events.Sum(e => e.Chance);
+
+        double normalization = totalChance > 1
+            ? 1 / totalChance
+            : 1;
+
+        double roll = _random.NextDouble();
+
+        double chanceSum = 0;
+
+        foreach (var @event in _events)
+        {
+            chanceSum += @event.Chance * normalization;
+
+            if (roll < chanceSum)
+                return @event.Action();
+        }
+
+        if (_fallback is not null)
+            return _fallback();
+
+        throw new InvalidOperationException("No event was drawn and no fallback was configured.");
+    }
+
+    private sealed record Event(double Chance, Func<T> Action);
+}
---
+++ DailyRugby.Tests/RandomEventListTests.cs
@@ -0,0 +1,29 @@
+﻿using DailyRugby.Application.Utilitaries;
+
+namespace DailyRugby.Tests;
+public class RandomEventListTests
+{
+    [Fact]
+    public void EventList_NoRollWithFallBack_RunsFallBack()
+    {
+        const string fallback = "This is the fallback";
+
+        RandomEventList<string> eventList = new(new Random());
+
+        eventList
+            .Add(0.5, () => "This has a 50% chance")
+            .Add(0.2, () => "This has a 20% chance")
+            .Add(0.1, () => "This has a 10% chance")
+            .AddFallback(() => "This is the fallback");
+
+        eventList
+            .Add(0.0, () => "Impossible 1")
+            .Add(0.0, () => "Impossible 2")
+            .Add(0.0, () => "Impossible 3")
+            .AddFallback(() => fallback);
+
+        string result = eventList.Draw();
+
+        Assert.Equal(fallback, result);
+    }
+}
