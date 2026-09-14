e5b1371f40f1a32882ad9110c1604c281700ff81
RafaX9 <rafaeldealencarfloriano@gmail.com>
2026-09-14 18:01:04 -0400

    Created SeasonThreeGameSimulator

---
+++ DailyRugby.Application/Simulators/SeasonThreeGameSimulator.cs
@@ -0,0 +1,17 @@
+﻿using DailyRugby.Application.Interfaces;
+using DailyRugby.Domain;
+
+namespace DailyRugby.Application.Simulators;
+
+public class SeasonThreeGameSimulator : ISpecificGameSimulator
+{
+    public Task SaveGameAsync(GameEvent gameEvent, AppDbContext db)
+    {
+        throw new NotImplementedException();
+    }
+
+    public GameEvent SimulateNextMinute(Game game)
+    {
+        throw new NotImplementedException();
+    }
+}
