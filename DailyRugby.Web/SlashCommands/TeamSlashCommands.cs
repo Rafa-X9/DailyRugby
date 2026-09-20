76dcade3a5496b6c28e9e6af9c5b777e374d0ea2
RafaX9 <rafaeldealencarfloriano@gmail.com>
2026-09-19 20:18:39 -0400

    Added the adding coach slash command

--- DailyRugby.Web/SlashCommands/TeamSlashCommands.cs
+++ DailyRugby.Web/SlashCommands/TeamSlashCommands.cs
@@ -218,6 +218,49 @@ public class TeamSlashCommands(ITeamCrudService teamService)
             $"T = {result.Item.Technique}", ephemeral: true);
     }
 
+    [SlashCommand("add-coach", "Add a coach to a team")]
+    public async Task AddCoachToTeam(
+        [Summary("team", "The team to add a coach to")]
+        [Autocomplete(typeof(TeamAutoComplete))]
+        string teamId,
+
+        [Summary("coach", "The coach to add")]
+        [Autocomplete(typeof(CoachAutoComplete))]
+        string coach)
+    {
+        if (!this.CheckRolePermission())
+        {
+            await RespondAsync(this.UnauthorizedMessage, ephemeral: true);
+            return;
+        }
+        await DeferAsync(ephemeral: true);
+
+        bool idParsed = Guid.TryParse(teamId, out Guid id);
+        if (!idParsed)
+        {
+            await FollowupAsync("Id isn't a valid Guid", ephemeral: true);
+            return;
+        }
+
+        bool coachParsed = Enum.TryParse(coach, true, out Coaches enumCoach);
+        if (!coachParsed)
+        {
+            await FollowupAsync("Invalid coach", ephemeral: true);
+            return;
+        }
+
+        var result = await teamService.AddCoachAsync(enumCoach, id);
+
+        if (!result.IsSuccessful)
+        {
+            await FollowupAsync($"{result.Error}: {result.Message}", ephemeral: true);
+            return;
+        }
+
+        await FollowupAsync($"Successfully added the {enumCoach} coach " +
+            $"to {result.Item.Country}", ephemeral: true);
+    }
+
     [SlashCommand("see-teams-json", "Get the teams from a championship as JSON")]
     public async Task SeeTeamsJson(
         [Summary("Championship", "The championship to get the teams from")]
