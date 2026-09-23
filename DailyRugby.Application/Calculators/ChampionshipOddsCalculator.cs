6fa4fce0fb4d90c0991f91871d7c26a35c05121c
RafaX9 <rafaeldealencarfloriano@gmail.com>
2026-09-23 16:05:25 -0400

    Added IChampionshipOddsCalculator

---
+++ DailyRugby.Application/Calculators/ChampionshipOddsCalculator.cs
@@ -0,0 +1,13 @@
+﻿using DailyRugby.Application.Interfaces;
+using DailyRugby.Domain;
+using DailyRugby.Shared;
+
+namespace DailyRugby.Application.Calculators;
+
+public class ChampionshipOddsCalculator : IChampionshipOddsCalculator
+{
+    public async Task<Result<ChampionshipOdds>> GetOddsAsync(Guid champId, bool passIfNotExists = false)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ DailyRugby.Application/Interfaces/IChampionshipOddsCalculator.cs
@@ -0,0 +1,9 @@
+﻿using DailyRugby.Domain;
+using DailyRugby.Shared;
+
+namespace DailyRugby.Application.Interfaces;
+
+public interface IChampionshipOddsCalculator
+{
+    Task<Result<ChampionshipOdds>> GetOddsAsync(Guid champId, bool passIfNotExists = false);
+}
