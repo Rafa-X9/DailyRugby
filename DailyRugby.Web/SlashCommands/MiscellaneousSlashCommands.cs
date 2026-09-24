1700b342303c151e4d43ac66e79ab56354ae9267
RafaX9 <rafaeldealencarfloriano@gmail.com>
2026-09-24 18:50:07 -0400

    Added slash command to see RAM usage

---
+++ DailyRugby.Web/SlashCommands/MiscellaneousSlashCommands.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Interactions;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace DailyRugby.Web.SlashCommands;
+
+public class MiscellaneousSlashCommands : InteractionModuleBase<SocketInteractionContext>
+{
+    [SlashCommand("see-ram-usage", "See how much RAM I am using")]
+    public async Task SeeRamUsage(
+        [Summary("Private", "Whether the response should be sent privately")]
+        bool @private = true)
+    {
+        long bytes = Process.GetCurrentProcess().PrivateMemorySize64;
+        double megaBytes = bytes / 1_000_000.0;
+        await RespondAsync($"I am using {megaBytes.ToString("F2", CultureInfo.InvariantCulture)} " +
+            $"megabytes of RAM", ephemeral: @private);
+    }
+}
